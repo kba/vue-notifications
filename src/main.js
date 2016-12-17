a8abae4882e0fb4217d381e0425c77e492b1e1a2
Sergey Panfilov <se-panfilov@ya.ru>
2016-12-17 19:59:12 +0100

    simplify

--- src/main.js
+++ src/main.js
@@ -12,13 +12,11 @@ const TYPE = {
 const VUE_VERSION = {
   1: {
     init: 'init',
-    destroy: 'beforeDestroy',
-    mounted: 'compiled'
+    destroy: 'beforeDestroy'
   },
   2: {
     init: 'beforeCreate',
-    destroy: 'beforeDestroy',
-    mounted: 'mounted'
+    destroy: 'beforeDestroy'
   },
 }
 
@@ -53,7 +51,11 @@ function showDefaultMessage ({ type, message, title, debugMsg }) {
 
 function getValues (vueApp, config) {
   const result = {}
-  const keepFnFields = ['cb', 'watch']
+  const keepFnFields = ['cb'] // TODO (S.Panfilov) any field can be a fn now!
+
+  // TODO (S.Panfilov) if {} we have to build object that extend properties from parents
+  // TODO (S.Panfilov) if func - pass function
+  // TODO (S.Panfilov) if string - pass {msg: 'string'} and etend from upper properties
 
   Object.keys(config).forEach(field => {
     keepFnFields.forEach(fnField => {
@@ -78,15 +80,15 @@ function showMessage (config, options, vueApp) {
   if (config.cb) return config.cb()
 }
 
-function addMethods (targetObj, typesObj, options) {
-  Object.keys(typesObj).forEach(v => {
-    targetObj[typesObj[v]] = function (config) {
-      config.type = typesObj[v]
-      // TODO (S.Panfilov)fix 'vueApp' in param
-      return showMessage(config, options)
-    }
-  })
-}
+// function addMethods (targetObj, typesObj, options) {
+//   Object.keys(typesObj).forEach(v => {
+//     targetObj[typesObj[v]] = function (config) {
+//       config.type = typesObj[v]
+//       // TODO (S.Panfilov)fix 'vueApp' in param
+//       return showMessage(config, options)
+//     }
+//   })
+// }
 
 function setMethod (vueApp, name, options, pluginOptions) {
   if (!options.methods) options.methods = {}
@@ -110,69 +112,51 @@ function makeMethod (vueApp, configName, options, pluginOptions) {
   }
 }
 
-function initVueNotificationPlugin (vueApp, notifications, pluginOptions) {
-  if (!notifications) return
-  Object.keys(notifications).forEach(name => {
-    setMethod(vueApp, name, vueApp.$options, pluginOptions)
-  })
+// function initVueNotificationPlugin (vueApp, notifications, pluginOptions) {
+//   if (!notifications) return
+//   Object.keys(notifications).forEach(name => {
+//     setMethod(vueApp, name, vueApp.$options, pluginOptions)
+//   })
+//
+//   vueApp.$emit(`${PACKAGE_NAME}-initiated`)
+// }
 
-  vueApp.$emit(`${PACKAGE_NAME}-initiated`)
-}
-
-function launchWatchableNotifications (vueApp, notifications) {
-  if (!notifications) return
-  Object.keys(notifications).forEach(name => {
-    if (vueApp[name] && notifications[name].watch) {
-      vueApp[name]()
-    }
-  })
-
-  vueApp.$emit(`${PACKAGE_NAME}-launched_watchable`)
-}
-
-function unlinkVueNotificationPlugin (vueApp, notifications) {
-  if (!notifications) return
-  const attachedMethods = vueApp.$options.methods
-  Object.keys(notifications).forEach(name => {
-    if (attachedMethods[name]) {
-      attachedMethods[name] = undefined
-      delete attachedMethods[name]
-    }
-  })
-
-  vueApp.$emit(`${PACKAGE_NAME}-unlinked`)
-}
+// function unlinkVueNotificationPlugin (vueApp, notifications) {
+//   if (!notifications) return
+//   const attachedMethods = vueApp.$options.methods
+//   Object.keys(notifications).forEach(name => {
+//     if (attachedMethods[name]) {
+//       attachedMethods[name] = undefined
+//       delete attachedMethods[name]
+//     }
+//   })
+//
+//   vueApp.$emit(`${PACKAGE_NAME}-unlinked`)
+// }
 
 
-function makeMixin (Vue, pluginOptions) {
-  const version = getVersion(Vue).major
-
-  // TODO (S.Panfilov) this?
-  // TODO (S.Panfilov) : function () { ?
-
-  return {
-    [VUE_VERSION[version].init]: function () {
-      const vueApp = this
-      const vueAppOptions = this.$options
-      const notificationsField = vueAppOptions[VueNotifications.propertyName]
-
-      initVueNotificationPlugin(vueApp, notificationsField, pluginOptions)
-    },
-    [VUE_VERSION[version].mounted]: function () {
-      const vueApp = this
-      const vueAppOptions = this.$options
-      const notificationsField = vueAppOptions[VueNotifications.propertyName]
-
-      launchWatchableNotifications(vueApp, notificationsField)
-    },
-    [VUE_VERSION[version].destroy]: function () {
-      const vueApp = this
-      const vueAppOptions = this.$options
-      const notificationsField = vueAppOptions[VueNotifications.propertyName]
-      unlinkVueNotificationPlugin(vueApp, notificationsField)
-    }
-  }
-}
+// function makeMixin (Vue, pluginOptions) {
+//   const version = getVersion(Vue).major
+//
+//   // TODO (S.Panfilov) this?
+//   // TODO (S.Panfilov) : function () { ?
+//
+//   return {
+//     [VUE_VERSION[version].init]: function () {
+//       const vueApp = this
+//       const vueAppOptions = this.$options
+//       const notificationsField = vueAppOptions[VueNotifications.propertyName]
+//
+//       initVueNotificationPlugin(vueApp, notificationsField, pluginOptions)
+//     },
+//     [VUE_VERSION[version].destroy]: function () {
+//       const vueApp = this
+//       const vueAppOptions = this.$options
+//       const notificationsField = vueAppOptions[VueNotifications.propertyName]
+//       unlinkVueNotificationPlugin(vueApp, notificationsField)
+//     }
+//   }
+// }
 
 const VueNotifications = {
   type: TYPE,
@@ -182,12 +166,21 @@ const VueNotifications = {
     timeout: 3000
   },
   installed: false,
+  show (config) {
+    // TODO (S.Panfilov) config has to be an Object
+    //example 1: this.$n.show(this.$n.login.success)
+    //example 2: this.$n.show(this.$n.error.login)
+    //example 3: this.$n.show({msg: 'asdasd'})
+  },
+  dismiss () {
+    // TODO (S.Panfilov) required for timeout 0
+  },
   install (Vue, pluginOptions = {}) {
     if (this.installed) throw console.error(MESSAGES.alreadyInstalled)
-    const mixin = makeMixin(Vue, pluginOptions)
-    Vue.mixin(mixin)
+    // const mixin = makeMixin(Vue, pluginOptions)
+    // Vue.mixin(mixin)
 
-    addMethods(this, this.type, pluginOptions)
+    // addMethods(this, this.type, pluginOptions)
 
     this.installed = true
   }
