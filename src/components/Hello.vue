1b70a9019ccc03555c2fe527bb1b19b0efd673e8
Sergey Panfilov <se-panfilov@ya.ru>
2016-12-17 19:16:37 +0100

    init and some new concept view

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -21,33 +21,82 @@
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    method: {
+      login () {
+        if (1) {
+          this.$n.login.success()
+//          this.$n(this.login.success)
+          this.$n.show(this.$n.login.success)
+          this.$n.show(this.$n.error.login)
+        }
+      }
+    },
+//    notifications: {
+//      successMsg: {
+//        type: 'success',
+//        message: 'some'
+//      }
+//    },
+    $n: {
+      login: {
+        timeout: 800,
+        title: 'Login: ',
+        success: {
+          type: 'success',
+          title: `${this.title} success`,
+          successOne: { msg: 'This is success', timeout: 300 },
+          successTwo: 'This is success',
+          successTree () {
+            return { msg: 'my msg', timeout: 400 }
+          }
+        },
+        error: {
+          type: 'error',
+          title: `${this.title} error`,
+          successOne: { msg: 'This is error' },
+          successTwo: 'This is error'
+        }
+      },
+      error: {
+        type: 'error',
+        timeout: 300,
+        login: {
+          msg: 'This is error',
+          title: 'error'
+        },
+        register: 'Some register error',
+        serverError () {
+          return 'Some register error'
+        }
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 </style>
